179bc8c3c50805885e757aaac3a7b6f090f1b864
luis correa <ylss512c@gmail.com>
2023-09-07 04:10:16 -0300

    logica terminada

--- Js/admin.js
+++ Js/admin.js
@@ -14,11 +14,12 @@ const printItems = async () => {
   productInfo.map((product) => {
     let containerCard = document.getElementById('cardContainer');
     let card = document.createElement('div');
-    card.classList = 'card col-3 m-3 g-0 justify-content-center cardProduct'
+    card.classList = `card col-3 m-3 g-0 justify-content-center cardProduct ${product.category}`
     card.innerHTML =`
     <img src="https://picsum.photos/200" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${product.name}</h5>
+      <h4 class="card-title">${product.category}</h4>
       <h4 class="card-title">${product.price}</h4>
       <a href="#" class="btn btn-primary">comprar</a>
     </div>`;
@@ -27,3 +28,24 @@ const printItems = async () => {
 }
 
 printItems();
+
+const printAllCategories = () => {
+  let elementos = document.querySelectorAll('.card')
+  elementos.forEach(x => {
+    x.style.display = 'flex';
+  });
+}
+
+const filterCategory1 = () => {
+  let elementos = document.querySelectorAll('.hola2')
+  elementos.forEach(x => {
+    x.style.display = 'none';
+  });
+}
+
+const filterCategory2 = () => {
+  let elementos = document.querySelectorAll('.hola')
+  elementos.forEach(x => {
+    x.style.display = 'none';
+  });
+}
